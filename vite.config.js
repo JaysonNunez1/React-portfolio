b051350ff2a55f8172fd8d79f4f8678d4be9d50d
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-08-13 08:35:45 -0400

    re deploying

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://github.com/JaysonNunez1/React-portfolio',
+  
   plugins: [react()],
   css: {
     modules: {
