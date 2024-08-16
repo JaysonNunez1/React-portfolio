47c4d2913ac96be082ccfd3b97e979472e92e617
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-08-15 22:12:56 -0400

    changed sizing

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,9 +7,11 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Jayson</h1>
-        <p className={styles.description}>
-        I am a Full-stack web developer with a certificate in full-stack web development from Columbia Engineering.
+        <h1 className={styles.title} style= {{ fontFamily: "GreyQo"}} >Hi, I'm Jayson</h1>
+        <p className={styles.description}
+        >
+        Full-stack web developer with a certificate in full-stack web development from Columbia Engineering.
+        I have proven skills in collaboration, organization, and both independent and team-based work.
         </p>
         <a href="mailto:jaysonnunez3@gmail.com" className={styles.contactBtn}>
           Contact Me
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">
+      <a className={styles.title} style= {{ fontFamily: "GreyQo"}} >
         Jayson's Portfolio!
       </a>
       <div className={styles.menu}>
